816b7f96c5de56d212a936e5017493eed1ef08e3
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-30 22:25:32 +0300

    chore: install antd-icons, add icon for theme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,9 @@
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 import { Button, Flex, Layout, Typography } from "antd";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
+import { stylesValue } from "@/lib/constants/styles-value";
+import { getTheme } from "@/lib/store/selectors/themeSelectors";
 import { toggleTheme } from "@/lib/store/slice/themeSlice";
 
 const { Header } = Layout;
@@ -8,10 +11,11 @@ const { Link } = Typography;
 
 export default function HeaderComponent() {
   const dispatch = useDispatch();
+  const theme = useSelector(getTheme);
 
   return (
     <Header>
-      <Flex justify="center">
+      <Flex gap={stylesValue.gapSmall}>
         <Link href="https://vite.dev" target="_blank">
           Vite
         </Link>
@@ -20,7 +24,9 @@ export default function HeaderComponent() {
         </Typography.Link>
       </Flex>
       <Flex>
-        <Button onClick={() => dispatch(toggleTheme())}>switch theme</Button>
+        <Button onClick={() => dispatch(toggleTheme())}>
+          {theme === "light" ? <SunOutlined /> : <MoonOutlined />}
+        </Button>
       </Flex>
     </Header>
   );
