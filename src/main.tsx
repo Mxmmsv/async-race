5c3887ff77ddd8c73e071bc623dfc48094e246c4
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-09-30 03:51:12 +0300

    feat: add custom theme for antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ConfigProvider } from "antd";
+import { customTheme } from "@/lib/theme/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider theme={customTheme}>
+      <App />
+    </ConfigProvider>
   </StrictMode>
 );
