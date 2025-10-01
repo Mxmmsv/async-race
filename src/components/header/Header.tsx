b76fb3e9b319e60c098e6959c6a3350ef42e5294
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-10-01 16:30:19 +0300

    style: some style improve

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 import { Button, Flex, Layout, Typography } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 
-import { stylesValue } from "@/lib/constants/styles-value";
+import { stylesValue } from "@/constants/styles-value";
 import { getTranslation } from "@/lib/store/selectors/i18nSelectors";
 import { getTheme } from "@/lib/store/selectors/themeSelectors";
 import { toggleTheme } from "@/lib/store/slice/themeSlice";
@@ -18,21 +18,23 @@ export default function HeaderComponent() {
 
   return (
     <Header>
-      <Flex gap={stylesValue.gapSmall}>
-        <Link href="https://vite.dev" target="_blank">
-          Vite
-        </Link>
-        <Link href="https://react.dev" target="_blank">
-          React
-        </Link>
-      </Flex>
-      <Flex gap={stylesValue.gapSmall}>
-        <Button onClick={() => dispatch(toggleTranslation())}>
-          {translation === "en" ? "EN" : "RU"}
-        </Button>
-        <Button onClick={() => dispatch(toggleTheme())}>
-          {theme === "light" ? <SunOutlined /> : <MoonOutlined />}
-        </Button>
+      <Flex align="center" justify="space-between" style={{ height: "100%" }}>
+        <Flex gap={stylesValue.gapSmall}>
+          <Link href="https://vite.dev" target="_blank">
+            Vite
+          </Link>
+          <Link href="https://react.dev" target="_blank">
+            React
+          </Link>
+        </Flex>
+        <Flex gap={stylesValue.gapSmall}>
+          <Button onClick={() => dispatch(toggleTranslation())}>
+            {translation === "en" ? "EN" : "RU"}
+          </Button>
+          <Button onClick={() => dispatch(toggleTheme())}>
+            {theme === "light" ? <SunOutlined /> : <MoonOutlined />}
+          </Button>
+        </Flex>
       </Flex>
     </Header>
   );
