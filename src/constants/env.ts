a670aac8943912ae4fac4d6b083009e7aef12c86
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-12-04 19:27:50 +0300

    feat: add environment variable definitions for base URL and cars per page

---
+++ src/constants/env.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const CARS_PER_PAGE = import.meta.env.VITE_CARS_PER_PAGE;
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,4 +2,5 @@
 
 interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
+  readonly VITE_CARS_PER_PAGE: number;
 }
